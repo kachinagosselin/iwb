82a63d313d5570bc666ee707eabcbbb81c46ec79
Kachina Gosselin <kachina@alum.mit.edu>
2013-04-23 18:52:20 -0700

    Add aws for paperclip and heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ MakerSource::Application.configure do
             :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
             :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
         }
+    }
 
 
   # Log the query plan for queries taking more than this (works
